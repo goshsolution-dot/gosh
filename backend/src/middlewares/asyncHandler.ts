6ac0b6dd3599116dd39a6bf05e765fbf7b408277
GodfreySilungwe <silungwegod@gmail.com>
2026-04-28 23:51:31 +0200

    cloudfront configured fixed

--- backend/src/middlewares/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
-
-export default function asyncHandler(fn: AsyncHandler) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
--- backend/src/middlewares/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export default function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  console.error(err);
-  res.status(500).json({
-    success: false,
-    message: err.message || 'Internal server error',
-  });
-}
